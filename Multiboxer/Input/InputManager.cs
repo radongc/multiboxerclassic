c86afd122977f9f1033329919030614d82def514
radongc <ccostanzo115@gmail.com>
2022-11-16 12:40:33 -0500

    Add license notices to source files - CC A-NC-SA 4.0 International

--- Multiboxer/Input/InputManager.cs
+++ Multiboxer/Input/InputManager.cs
@@ -1,4 +1,14 @@
 ﻿/*
+    Multiboxer Classic (c) by Radon (https://github.com/radongc)
+
+    Multiboxer Classic is licensed under a
+    Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License.
+
+    You should have received a copy of the license along with this
+    work. If not, see <http://creativecommons.org/licenses/by-nc-sa/4.0/>.
+*/
+
+/*
  * InputManager.cs
  * The core of the multiboxing functionality. This class handles all key hooking and broadcasting. Needs to be rewritten as soon as possible as it is messy.
  */
--- Multiboxer/MainForm.Designer.cs
+++ Multiboxer/MainForm.Designer.cs
@@ -1,4 +1,14 @@
-﻿namespace Multiboxer
+﻿/*
+    Multiboxer Classic (c) by Radon (https://github.com/radongc)
+
+    Multiboxer Classic is licensed under a
+    Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License.
+
+    You should have received a copy of the license along with this
+    work. If not, see <http://creativecommons.org/licenses/by-nc-sa/4.0/>.
+*/
+
+namespace Multiboxer
 {
     partial class MainForm
     {
